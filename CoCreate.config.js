3e9a50fe7e8758de5dbaa427856eb88651539032
Frank Pagan <frank@cocreate.app>
2022-11-21 19:22:01 +0000

    fix: replaced document_id with document._id

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -10,7 +10,7 @@ module.exports = {
             "collection": "files",
             "document_id": "6025f32c7a78a543a77c921e",
             "key": "src",
-            "data":{
+            "document": {
                 "name": "index.html",
                 "path": "/docs/sandbox/index.html",
                 "domains": [
